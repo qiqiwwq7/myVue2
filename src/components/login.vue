01d6cc065564bf388df3fae948e57bffa2cb61ca
relign <727510812@qq.com>
2017-08-18 00:41:01 +0800

    update

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,39 +1,41 @@
 <template>
   <div class="login">
-    <el-form :inline="true" :model="formInline" class="demo-form-inline">
-      <el-form-item label="审批人">
-      <el-input v-model="formInline.user" placeholder="审批人"></el-input>
-    </el-form-item>
-    <el-form-item label="活动区域">
-    <el-select v-model="formInline.region" placeholder="活动区域">
-      <el-option label="区域一" value="shanghai"></el-option>
-      <el-option label="区域二" value="beijing"></el-option>
-    </el-select>
-    </el-form-item><el-form-item>
-      <el-button type="primary" @click="onSubmit">查询</el-button>
-    </el-form-item>
-  </el-form>
-  <div class='el-icon-loading'></div>
+    <h1 v-text='title'></h1>
+    <ul>
+      <li v-for='item in items' :class='{finished: item.isFinishes}'>{{item.label}}</li>
+    </ul>
   </div>
 </template>
 <script>
   export default {
     data () {
       return {
-        formInline: {
-          user: '',
-          region: ''
-        }
+        // kk
+        title: 'this is a todo list',
+        items: [
+          {
+            label: 'coding',
+            isFinished: false,
+            isFinishes: false
+          },
+          {
+            label: 'coding',
+            isFinished: true,
+            isFinishes: true
+          }
+        ],
+        liClass: 'thisisliClass'
       }
     },
     methods: {
-      onSubmit () {
-        console.log('submit!')
-      }
+      // kk
     }
   }
 </script>
 <style>
+.finished {
+  text-decoration: underline;
+}
 
 </style>
 
